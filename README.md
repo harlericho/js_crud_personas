677c1bcd19add1e183405ea7295965586c15c5d9
charlie <harlericho@gmail.com>
2021-09-20 17:34:22 -0500

    terminado

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# js_crud_personas
+# JAVASCRIPT 
+[![JavaScript](https://img.shields.io/badge/--F7DF1E?logo=javascript&logoColor=000)](https://www.javascript.com/)
+[![Visual Studio](https://badgen.net/badge/icon/visualstudio?icon=visualstudio&label)](https://visualstudio.microsoft.com)
+[![GitHub](https://badgen.net/badge/icon/github?icon=github&label)](https://github.com/harlericho)
+
 Crud de los datos de una persona, usando python como backend
+![](https://dermotblog.netlify.app/static/1fedb9281ec46d21e3bfbd075eb765eb/5ca85/js.png)
+
+[![Windows](https://svgshare.com/i/ZhY.svg)](https://www.microsoft.com/es-es/windows)
+[![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](https://github.com/Naereen/StrapDown.js/blob/master/LICENSE)
+[![Twitter](https://badgen.net/badge/icon/twitter?icon=twitter&label)](https://twitter.com/harlericho)
+[![Open in Visual Studio Code](https://open.vscode.dev/badges/open-in-vscode.svg)](https://open.vscode.dev/Naereen/badges)
+[![Website myfakewebsitethatshouldnotexist.at.least.i.hope](https://img.shields.io/website-up-down-green-red/http/myfakewebsitethatshouldnotexist.at.least.i.hope.svg)](https://harlericho.github.io/portafolio/)
+[![made-with-javascript](https://img.shields.io/badge/Made%20with-JavaScript-1f425f.svg)](https://www.javascript.com)
+
+
+
+# Pasos para funcionar el sistema:
+### Clonar el repositorio hecho en python:
+> https://github.com/harlericho/python_api_personas.git
+
+### Ejecutar en el proyecto el index.html:
+```sh
+index.html => desde el navegador preferido
+index.html => en vscode ejecutar con live serve (extensión)
+```
+
+## _Realizado por:_
+![](https://avatars.githubusercontent.com/u/42042270?s=48&v=4)
+
+# Github: @harlericho
+
+> The MIT License (MIT)
+
+### Este programa o sistema puede ser tomado como guia o enseñanza para sus futuros  proyectos.
+> Copyright (c) 2021 harlericho
---
+++ code/index.js
@@ -0,0 +1,140 @@
+var app = new function () {
+    const url = "http://localhost:5000/"
+    this.id = document.getElementById("id")
+    this.nombres = document.getElementById("nombres")
+    this.dni = document.getElementById("dni")
+    this.fecha = document.getElementById("fecha")
+    this.correo = document.getElementById("correo")
+    this.tbody = document.getElementById("tbody")
+
+    this.listado = () => {
+        fetch(url)
+            .then(response => response.json())
+            .then((data) => {
+                html = []
+                data.datos.forEach(element => {
+                    html += "<tr>"
+                    html += "<td>" + (element['id'] - 2) + "</td>"
+                    html += "<td>" + element['dni'] + "</td>"
+                    html += "<td>" + element['nombres'] + "</td>"
+                    html += "<td>" + element['fecha'] + "</td>"
+                    html += "<td>" + element['correo'] + "</td>"
+                    html += "<td><button type='button' class='btn btn-success btn-sm'  onClick='app.obtener(" + element['id'] + ")'>Editar</button></td>"
+                    html += "<td><button type='button' class='btn btn-danger btn-sm' onClick='app.eliminar(" + element['id'] + ")'>Eliminar</button></td>"
+                    html += "</tr>"
+                })
+                this.tbody.innerHTML = html
+            })
+            .catch(error => console.error(error))
+    }
+    this.guardar = () => {
+        const info = {
+            dni: this.dni.value,
+            nombres: this.nombres.value,
+            fecha: this.fecha.value,
+            correo: this.correo.value
+        }
+        if (this.id.value === "") {
+            fetch(url, {
+                    method: "POST",
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(info)
+                })
+                .then(response => response.json())
+                .then((data) => {
+                    if (data.dni) {
+                        alertify.set('notifier', 'position', 'top-right');
+                        alertify.error(data.dni);
+                        this.dni.focus()
+                    } else if (data.correo) {
+                        alertify.set('notifier', 'position', 'top-right');
+                        alertify.error(data.correo);
+                        this.correo.focus()
+                    } else {
+                        alertify.set('notifier', 'position', 'top-right');
+                        alertify.success(data.datos);
+                        this.listado()
+                    }
+                })
+                .catch(error => console.error(error))
+        } else {
+            fetch(url + this.id.value, {
+                    method: "PUT",
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(info)
+                })
+                .then(response => response.json())
+                .then((data) => {
+                    if (data.dni) {
+                        alertify.set('notifier', 'position', 'top-right');
+                        alertify.error(data.dni);
+                        this.dni.focus()
+                    } else if (data.correo) {
+                        alertify.set('notifier', 'position', 'top-right');
+                        alertify.error(data.correo);
+                        this.correo.focus()
+                    } else {
+                        alertify.set('notifier', 'position', 'top-right');
+                        alertify.success(data.datos);
+                        this.listado()
+                    }
+                })
+                .catch(error => console.error(error))
+        }
+        this.limpiar()
+    }
+    this.limpiar = () => {
+        document.getElementById("formulario").reset()
+        this.dni.focus()
+    }
+    this.obtener = (id) => {
+        fetch(url + id, {
+                method: "GET",
+            })
+            .then(response => response.json())
+            .then((data) => {
+                this.id.value = data.datos['id']
+                this.dni.value = data.datos['dni']
+                this.nombres.value = data.datos['nombres']
+                this.fecha.value = data.datos['fecha']
+                this.correo.value = data.datos['correo']
+                alertify.set('notifier', 'position', 'top-right');
+                alertify.warning("Usted va a actualizar los datos");
+            })
+            .catch(error => console.error(error))
+    }
+    this.eliminar = (id) => {
+        Swal.fire({
+            title: 'Estás seguro?',
+            text: "No podrás revertir esto!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, eliminarlo!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Eliminado!',
+                    'Su archivo ha sido eliminado',
+                    'success'
+                )
+                fetch(url + id, {
+                        method: "DELETE",
+                    })
+                    .then(response => response.json())
+                    .then((data) => {
+                        alertify.set('notifier', 'position', 'top-right');
+                        alertify.success(data.datos);
+                        this.listado()
+                    })
+                    .catch(error => console.error(error))
+            }
+        })
+    }
+}
+app.listado()
